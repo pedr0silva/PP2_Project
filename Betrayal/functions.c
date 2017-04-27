589b794a7b7970ce6e4c7ae0874b4ca9fd1bf1ef
Pedro Silva <pmiguelfs@gmail.com>
2017-04-27 20:50:40 +0100

    Updating Functions

--- Betrayal/functions.c
+++ Betrayal/functions.c
@@ -15,6 +15,7 @@ Date Log:
 */
 
 #include <stdio.h>
+
 #include "estructs.h"
 
 //Allocates memory for a Map and gives value to it's atributes.
@@ -32,33 +33,244 @@ MapPtr CreateMap(Floor mapFloor)
 }
 
 //Allocates memory for a Room and gives value to it's atributes.
-RoomPtr CreateRoom(string roomName, EventPtr roomEvent, OmenPtr roomOmen)
+/*RoomPtr CreateRoom(string roomName, EventPtr roomEvent, OmenPtr roomOmen)
 {
 	RoomPtr aux = (RoomPtr)malloc(sizeof(Room));
 
 	aux->name = roomName;
 	aux->event = roomEvent;
 	aux->omen = roomOmen;
-	//aux->characterList = NULL;
-	//aux->next = NULL;
+	aux->next = NULL;
 
 	return aux;
-}
+}*/
 
 //Allocates memory for a Character and gives value to it's atributes.
-CharacterPtr CreateChar(string charName, int charMight, int charSpeed, int charSanity, int charInteligence)
+CharacterPtr CreateChar(string name, int might, int speed, int sanity, int inteligence)
 {
 	CharacterPtr aux = (CharacterPtr)malloc(sizeof(Character));
 
-	aux->name = charName;
-	aux->might = charMight;
-	aux->speed = charSpeed;
-	aux->sanity = charSanity;
-	aux->inteligence = charInteligence;
+	aux->name = name;
+	aux->might = might;
+	aux->speed = speed;
+	aux->sanity = sanity;
+	aux->inteligence = inteligence;
 	aux->history = NULL;
 	aux->minions = NULL;
 	aux->items = NULL;
+	aux->room = NULL;
 	aux->next = NULL;
 	
 	return aux;
 }
+
+//Allocates memory for an Event and gives value to it's atributes.
+EventPtr CreateEvent(string name, string description, int might_mod, int speed_mod, int sanity_mod, int inteligence_mod)
+{
+	EventPtr aux = (EventPtr)malloc(sizeof(Event));
+
+	aux->name = name;
+	aux->description = description;
+	aux->might_mod = might_mod;
+	aux->speed_mod = speed_mod;
+	aux->sanity_mod = sanity_mod;
+	aux->intellect_mod = inteligence_mod;
+	aux->next = NULL;
+
+	return aux;
+}
+
+//Allocates memory for an Omen and gives value to it's atributes.
+OmenPtr CreateOmen(string name, string description, int might_mod, int speed_mod, int sanity_mod, int inteligence_mod)
+{
+	OmenPtr aux = (OmenPtr)malloc(sizeof(Omen));
+
+	aux->name = name;
+	aux->description = description;
+	aux->might_mod = might_mod;
+	aux->speed_mod = speed_mod;
+	aux->sanity_mod = sanity_mod;
+	aux->intellect_mod = inteligence_mod;
+	aux->next = NULL;
+
+	return aux;
+}
+
+//Allocates memory for an Item and gives value to it's atributes.
+ItemPtr CreateItem(string name, string description, int might_mod, int speed_mod, int sanity_mod, int inteligence_mod)
+{
+	ItemPtr aux = (ItemPtr)malloc(sizeof(Item));
+
+	aux->name = name;
+	aux->description = description;
+	aux->might_mod = might_mod;
+	aux->speed_mod = speed_mod;
+	aux->sanity_mod = sanity_mod;
+	aux->intellect_mod = inteligence_mod;
+	aux->next = NULL;
+
+	return aux;
+}
+
+
+#pragma region Auxiliar functions
+
+void AddCharacterCards(Character infoChar[])
+{
+	string name;
+	int might, speed, sanity, inteligence;
+
+	FILE *f = fopen("cards.bin", "ab");
+	
+
+	for (int i = 0; i < MAX_CHARACTERS; i++)
+	{
+		printf("Insert characters name: ");
+		scanf("%s", name);
+		printf("\n");
+		printf("Might: ");
+		scanf("%d", might);
+		printf("\n");
+		printf("Speed: ");
+		scanf("%d", speed);
+		printf("\n");
+		printf("Sanity: ");
+		scanf("%d", sanity);
+		printf("\n");
+		printf("Inteligence: ");
+		scanf("%d", inteligence);
+		printf("\n");
+
+		CharacterPtr newChar = CreateChar(name, might, speed, sanity, inteligence);
+		infoChar[i] = *newChar;
+
+
+		fwrite(newChar, sizeof(Character), 1, f);
+	}
+
+	fclose(f);
+}
+
+void AddEventCards(Event infoEvent[])
+{
+	string name, description;
+	int might_mod, speed_mod, sanity_mod, inteligence_mod;
+
+	FILE *f = fopen("cards.bin", "ab");
+
+	for (int i = 0; i < MAX_EVENTS; i++)
+	{
+		printf("Insert event name: ");
+		scanf("%s", name);
+		printf("\n");
+		printf("Insert the event's description: ");
+		scanf("%s", description);
+		printf("\n");
+		printf("Might: ");
+		scanf("%d", might_mod);
+		printf("\n");
+		printf("Speed: ");
+		scanf("%d", speed_mod);
+		printf("\n");
+		printf("Sanity: ");
+		scanf("%d", sanity_mod);
+		printf("\n");
+		printf("Inteligence: ");
+		scanf("%d", inteligence_mod);
+		printf("\n");
+
+		EventPtr newEvent = CreateEvent(name, description, might_mod, speed_mod, sanity_mod, inteligence_mod);
+		infoEvent[i] = *newEvent;
+
+		fwrite(newEvent, sizeof(Event), 1, f);
+	}
+
+	fclose(f);
+}
+
+void AddOmenCards(Omen infoOmen[])
+{
+	string name, description;
+	int might_mod, speed_mod, sanity_mod, inteligence_mod;
+
+	FILE *f = fopen("cards.bin", "ab");
+
+	for (int i = 0; i < MAX_OMENS; i++)
+	{
+		printf("Insert omen name: ");
+		scanf("%s", name);
+		printf("\n");
+		printf("Insert the omen's description: ");
+		scanf("%s", description);
+		printf("\n");
+		printf("Might: ");
+		scanf("%d", might_mod);
+		printf("\n");
+		printf("Speed: ");
+		scanf("%d", speed_mod);
+		printf("\n");
+		printf("Sanity: ");
+		scanf("%d", sanity_mod);
+		printf("\n");
+		printf("Inteligence: ");
+		scanf("%d", inteligence_mod);
+		printf("\n");
+
+		OmenPtr newOmen = CreateOmen(name, description, might_mod, speed_mod, sanity_mod, inteligence_mod);
+		infoOmen[i] = *newOmen;
+
+		fwrite(newOmen, sizeof(Omen), 1, f);
+
+	}
+
+	fclose(f);
+}
+
+void AddItemCards(Item infoItem[])
+{
+	string name, description;
+	int might_mod, speed_mod, sanity_mod, inteligence_mod;
+
+	for (int i = 0; i < MAX_ITEMS; i++)
+	{
+		printf("Insert item name: ");
+		scanf("%s", name);
+		printf("\n");
+		printf("Insert the card's description: ");
+		scanf("%s", description);
+		printf("\n");
+		printf("Might: ");
+		scanf("%d", might_mod);
+		printf("\n");
+		printf("Speed: ");
+		scanf("%d", speed_mod);
+		printf("\n");
+		printf("Sanity: ");
+		scanf("%d", sanity_mod);
+		printf("\n");
+		printf("Inteligence: ");
+		scanf("%d", inteligence_mod);
+		printf("\n");
+
+		ItemPtr newItem = CreateItem(name, description, might_mod, speed_mod, sanity_mod, inteligence_mod);
+		infoItem[i] = *newItem;
+	}
+}
+
+//Uses the functions above to ask for card information and add it to the database
+void AddCards(Card c)
+{
+	AddCharacterCards(c.characterList);
+	AddEventCards(c.eventList);
+	AddOmenCards(c.omenList);
+	AddItemCards(c.itemList);
+
+	/*FILE *f = fopen("cards.bin", "wb");
+
+	for (int i = 0; i < MAX_CHARACTERS; i++)
+	{
+		fwrite(&c.characterList[i], sizeof(Character), 1, f);
+	}*/
+
+}
+#pragma endregion
