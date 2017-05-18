80ae96db9fe948866d5b35ddc8c6cd423c6f3e59
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-05-18 01:06:17 +0100

    Options Beg

--- Betrayal/graphics.c
+++ Betrayal/graphics.c
@@ -4,16 +4,14 @@
 
 void ReadInput(string input)
 {
-	printf("Input: ");
+	printf("INPUT: ");
 	gets(input);
 	strupr(input);
-	system("cls");
 }
 void InputBreak()
 {
-	printf("Press a key to continue.");
+	printf("PRESS A KEY TO CONTINUE.");
 	getch();
-	system("cls");
 }
 
 boolean InsertLineInDrawingTable(char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH], Vector2Ptr position, string text)
@@ -36,6 +34,7 @@ boolean InsertLineInDrawingTable(char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH], Vec
 }
 boolean CleanDrawingTable(char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
 {
+	system("cls");
 	for (int i = 0; i < MAX_HEIGHT; i++)
 		for (int j = 0; j < MAX_WIDTH; j++)
 			(*drawingTable)[i][j] = ' ';
@@ -43,23 +42,52 @@ boolean CleanDrawingTable(char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
 
 int UpdateMap(Master *mainStruct, char *drawingTable[MAX_HEIGHT][MAX_WIDTH])
 {
-	
+
 }
-int DrawMap(char (*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
+int DrawMap(char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
 {
 	for (int i = 0; i < MAX_HEIGHT; i++)
 		for (int j = 0; j < MAX_WIDTH; j++)
 			printf("%c", (*drawingTable)[i][j]);
 }
-void Menu(MasterPtr master, char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
+
+void Options(MasterPtr master, char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
 {
 	string inputStr;
 	InitString(inputStr);
-	string auxStr = "WELCOME TO BETRAYAL AT HOUSE ON THE HILL";
 
+	string auxStr;
+	Vector2Ptr auxPos = CreateVector2(0, 0);
+
+	do
+	{
+		CleanDrawingTable(*drawingTable);
+
+		strcpy(auxStr, "CHARACTERS");
+		auxPos = ChangeVector2(auxPos, 35, 14);
+		InsertLineInDrawingTable(*drawingTable, auxPos, auxStr);
+
+		strcpy(auxStr, "BACK");
+		auxPos = ChangeVector2(auxPos, 35, 16);
+		InsertLineInDrawingTable(*drawingTable, auxPos, auxStr);
+
+		DrawMap(*drawingTable);
+		ReadInput(inputStr);
+
+	} while ((strcmp(inputStr, "BACK") != 0));
+}
+
+void Menu(MasterPtr master, char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
+{
+	string inputStr;
+	string auxStr;
 	Vector2Ptr auxPos = CreateVector2(40, 14);
+	boolean error = FALSE;
+	InitString(inputStr);
+
+	strcpy(auxStr, "WELCOME TO BETRAYAL AT HOUSE ON THE HILL");
 	InsertLineInDrawingTable(*drawingTable, auxPos, auxStr);
-	DrawMap(*drawingTable);	
+	DrawMap(*drawingTable);
 
 	InputBreak();
 	do
@@ -78,8 +106,25 @@ void Menu(MasterPtr master, char(*drawingTable)[MAX_HEIGHT][MAX_WIDTH])
 		auxPos = ChangeVector2(auxPos, 35, 18);
 		InsertLineInDrawingTable(*drawingTable, auxPos, auxStr);
 
+		if (error == TRUE)
+		{
+			strcpy(auxStr, "WRONG INPUT PLEASE TRY AGAIN");
+			auxPos = ChangeVector2(auxPos, 0, MAX_HEIGHT - 1);
+			InsertLineInDrawingTable(*drawingTable, auxPos, auxStr);
+			error = FALSE;
+		}
+
 		DrawMap(*drawingTable);
 		ReadInput(inputStr);
+
+		if (strcmp(inputStr, "PLAY") == 0)
+			break;
+		//Play(master, drawingTable);
+		else if (strcmp(inputStr, "OPTIONS") == 0)
+			Options(master, drawingTable);
+		else if (strcmp(inputStr, "EXIT") != 0)
+			error = TRUE;
+	
 	} while ((strcmp(inputStr, "EXIT") != 0));
 
 	CleanDrawingTable(*drawingTable);
